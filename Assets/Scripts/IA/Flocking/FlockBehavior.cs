cacbdb33da82e0e602f047795788fd1e32fd193d
Bárbara Lucia Lombardo <barbara.lombardo.98@gmail.com>
2022-10-22 21:14:38 -0300

    feat: Project cleanup.

--- Assets/Scripts/IA/Flocking/FlockBehavior.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class FlockBehavior : MonoBehaviour
-{
-    public abstract Vector3 CalculateDirection(List<Transform> context);
-}
